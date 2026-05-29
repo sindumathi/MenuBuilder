68842b545faced8e76a64939d73deb26e9684ad5
sindumathi <kmsindumathi@gmail.com>
2026-05-28 21:43:42 -0500

    ADD:Calculate total price and add and remove pizza functionality

--- src/app/page.js
+++ src/app/page.js
@@ -9,13 +9,35 @@ import Menu from "./menu";
 import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [totalPrice, setTotalPrice] = useState(0);
+  function updateTotalPrice(price, isAdd) {
+    if (isAdd) {
+      setTotalPrice((totalPrice) => totalPrice + price);
+    } else {
+      setTotalPrice((totalPrice) => totalPrice - price);
+    }
+  }
+
+  function handleOrder() {
+    if (totalPrice > 0) {
+      alert("You placed your order");
+    } else {
+      alert("Please add pizza to order!!!");
+    }
+  }
   return (
     <div className={styles.homeContainer}>
       <Header />
       <Menu />
+      <div className={styles.priceContainer}>
+        <span id={styles.totalPrice}>{`Total: $${totalPrice}`}</span>
+        <button id={styles.order} onClick={handleOrder}>
+          Order
+        </button>
+      </div>
       <div className={styles.outerContainer}>
         {pizzaData.map((data) => (
-          <Pizza key={data.id} data={data} />
+          <Pizza key={data.id} data={data} updatePrice={updateTotalPrice} />
         ))}
       </div>
       <Footer />
@@ -24,13 +46,28 @@ export default function Home() {
 }
 
 function Pizza(props) {
-  const { data } = props;
+  const { data, updatePrice } = props;
+  const [count, setCount] = useState(0);
+  function handleAdd() {
+    if (count >= 10) {
+      alert("You can order only 10 items per order");
+    } else {
+      setCount((count) => count + 1);
+      updatePrice(data.price, true);
+    }
+  }
+  function handleRemove() {
+    if (count >= 1) {
+      updatePrice(data.price, false);
+      setCount((count) => count - 1);
+    }
+  }
   return (
     <div className={styles.itemContainer}>
       <div className={styles.imageContainer}>
         <img src={data.photoName} alt={data.name} />
       </div>
-      <div className="contentContainer">
+      <div className={styles.contentContainer}>
         <div className={styles.headerText}>{data.name}</div>
         <div className={`${styles.centerText} ${styles.smallText}`}>
           {data.ingredients}
@@ -39,6 +76,15 @@ function Pizza(props) {
           className={`${styles.alignContainerRight} ${styles.priceText} ${styles.maroonText}`}
         >
           {`$${data.price}`}
+        </div>
+        <div className={styles.orderContainer}>
+          <span id={styles.add} onClick={() => handleAdd(data)}>
+            +
+          </span>
+          <span id={styles.total}>{count}</span>
+          <span id={styles.minus} onClick={() => handleRemove(data)}>
+            -
+          </span>
         </div>
       </div>
     </div>
